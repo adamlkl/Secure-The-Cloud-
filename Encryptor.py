d0aeb8213b14043ec99bc86abe4d26d8ab2838ad
adamlkl <kleong@tcd.ie>
2019-03-29 15:56:19 +0000

    ENCRYPT

--- Encryptor.py
+++ Encryptor.py
@@ -5,4 +5,20 @@ Created on Fri Mar 29 14:35:29 2019
 
 @author: Adamlkl
 """
+from cryptography.fernet import Fernet
 
+class Encryptor:
+    
+    def generate_key(self):
+        key = Fernet.generate_key()
+        return key
+                
+    def encrypt(self, file, key):
+        cipher_suite = Fernet(key)
+        encrypted_file = cipher_suite.encrypt(file)
+        return encrypted_file
+        
+    def decrypt(self, encrypted_file, key):
+        cipher_suite = Fernet(key)
+        decrypted_file = cipher_suite.decrypt(encrypted_file)
+        return decrypted_file
---
+++ KeySaver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 15:51:42 2019
+
+@author: Adamlkl
+""" 
+''' 
+    - https://www.datacamp.com/community/tutorials/pickle-python-tutorial#what 
+    - https://security.stackexchange.com/questions/12332/where-to-store-a-server-side-encryption-key
+'''
+
+class KeySaver:
+    
+    
