3c5dbb66bad54211e9640088119f7c753f22ccc6
adamlkl <kleong@tcd.ie>
2019-04-06 11:03:49 +0100

    gitignore

---
+++ CloudGroup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 13:19:19 2019
+
+@author: Adamlkl
+"""
+
--- KeySaver.py
+++ KeySaver.py
@@ -10,8 +10,14 @@ Created on Fri Mar 29 15:51:42 2019
     - https://security.stackexchange.com/questions/12332/where-to-store-a-server-side-encryption-key
     - https://pypi.org/project/pyAesCrypt/
 '''
+from cryptography.hazmat.primitives import serialization
+
 class KeySaver:
     
-    def save_key():
-        
+    def save_key(private_key):
+        pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.BestAvailableEncryption(b'mypassword')
+        )
     def get_key():
